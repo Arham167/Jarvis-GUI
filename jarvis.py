e3b8c7175137739541dd9c0a65ed13cc3e5bf6ec
Arham167 <arhamnoman17@gmail.com>
2024-10-25 19:04:58 +0500

    Added a username and password field, and a new file that has a dictionary to store and check the usernames and passwords

--- jarvis.py
+++ jarvis.py
@@ -1,7 +1,8 @@
-import PIL, requests
+import PIL, requests, users
 from tkinter import *
 from PIL import ImageTk, Image
 from io import BytesIO
+from functools import partial
 
 # initialize the main screen
 root = Tk()
@@ -43,8 +44,33 @@ canvas.pack(side = "bottom")
 canvas.create_image(1, 1, anchor = NW, image = img)
 
 def main():
+    canvas.create_text(w/1.95, h/5, 
+                       text = "Enter Username & Password", 
+                       font = ("Lucida Fax", int(w/38)),
+                       fill = "white")
+    user_area = Entry(canvas,
+                    font = ("Lucida Fax", int(w/38)))
+    user_area.place(x = int (w/3.5), y = int(h/3))
+    username = user_area.get()
+    pwd_area = Entry(canvas,
+                    font = ("Lucida Fax", int(w/38)))
+    pwd_area.place(x = int (w/3.5), y = int(h/2))
+    pwd = pwd_area.get()
+    bt1 = Button(canvas,
+                 text = "Sign In",
+                 font = ("Arial Greek", int(w/80)),
+                 cursor = "mouse",
+                 command = partial(user_auth, username, pwd))
+    bt1.place(x = int (w/3.5), y = int(h/1.5))
     
     root.mainloop()
+
+def user_auth(username, pwd):
+    print(users.auth_users)
+    if username and pwd in users.auth_users:
+        print("ok")
+    else:
+        print("no")
 
 if __name__ == "__main__":
     main()
